ab9c27d5c3ece52ab0f7c0b35d0e2508420b8fc2
DongTran <tdong3110@gmail.com>
2021-02-21 18:59:34 +0700

    I done this so long time ago and now i have to push it to git Signed-off-by: DongTran <tdong3110@gmail.com>

--- Exercises Lunner New Year/JQ1/Ex2/script.js
+++ Exercises Lunner New Year/JQ1/Ex2/script.js
@@ -1,11 +1,10 @@
 function get() {
     var a = document.getElementById("a").nodeValue;
-    var tmp = "";
     for(var i=1; i<=a; i++){
         if(a%i==0){
-            tmp = a;
+            document.getElementById("chan").innerHTML=i;
         }
+
     }
-    document.getElementById("chan").innerHTML=a;
     
 }
